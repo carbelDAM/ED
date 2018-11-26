8dac75bed184770ede7b295d325f176efed376aa
carbelDAM <1dam.cgalicia@institutoserpis.org>
2018-11-26 09:02:51 +0100

    Añadido

---
+++ CSuma/CSuma/CSuma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSuma
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("-------Integer--------");
+                int num1 = Convert.ToInt16(Console.ReadLine());
+                int num2 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(num1 + num2);
+                Console.WriteLine("-------Decimal--------");
+                decimal num3 = decimal.Parse(Console.ReadLine());
+                decimal num4 = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(num3 + num4);
+        }
+        
+    }
+}
+    
+
