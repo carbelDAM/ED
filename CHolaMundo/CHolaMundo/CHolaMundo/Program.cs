9e484691ddb4dfe7d686e9f07a73ae4c00ded93b
carbelDAM <1dam.cgalicia@institutoserpis.org>
2018-11-26 09:01:26 +0100

    Añadido

---
+++ CHolaMundo/CHolaMundo/CHolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CHolaMundo
+{
+    class MainClass
+    {
+        public static void Main(string [] args){
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola "+nombre);
+
+        }
+    }
+}
